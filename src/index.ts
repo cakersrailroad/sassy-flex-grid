1759f0bf666e93dea160aeb07b09d7c527ee5bf1
Faisal Nawaz <fslnwz0@gmail.com>
2016-11-17 12:19:16 -0500

    added filters

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-/// <reference path="../typings/index.d.ts" />
+/// <reference path="../typings/index.d.ts"" />
 
 import * as angular from 'angular';
+require('../node_modules/sassy-numbers-utility/numbers/numbers.module.js'); // eslint-disable-line babel/no-var-requires
+require('../node_modules/sassy-numbers-utility/numbers/convertStringToNumber.filter.js'); // eslint-disable-line babel/no-var-requires
+require('../node_modules/sassy-numbers-utility/numbers/currencyOldStyle.filter.js'); // eslint-disable-line babel/no-var-requires
+require('../node_modules/sassy-numbers-utility/numbers/numberConversion.service.js'); // eslint-disable-line babel/no-var-requires
+require('../node_modules/sassy-numbers-utility/numbers/numberOldStyle.filter.js'); // eslint-disable-line babel/no-var-requires
+require('../node_modules/sassy-numbers-utility/numbers/percentOldStyle.filter.js'); // eslint-disable-line babel/no-var-requires
 
 import {hello} from './app/hello';
 import {flexGrid} from './components/flexGrid/flexGrid';
@@ -10,6 +16,6 @@ import './index.scss';
 export const app: string = 'app';
 
 angular
-  .module(app, [])
+  .module(app, ['NumbersUtil'])
   .component('app', hello)
   .component('flexGrid', flexGrid);
