ad5abe284f7b20e56eb7a6e9c3832552d6cd374f
Faisal Nawaz <fslnwz0@gmail.com>
2016-11-22 12:12:37 -0500

    added portal modeling jsgon

--- src/app/hello.ts
+++ src/app/hello.ts
@@ -1,26 +1,25 @@
 export const hello: angular.IComponentOptions = {
   template: require('./hello.html'),
   controller: function ($http: angular.IHttpService, $scope: angular.IScope) { // eslint-disable-line babel/object-shorthand
-    var vm = this;
-    this.hello = 'Hello World!';
-    // let data = [];
+
     $http.get('/assets/db.json').then(dat => this.data = dat.data);
+    $http.get('/assets/portal_modeling.json').then(dat => this.portalModeling = dat.data);
 
 
     this.callBackApi = {
       callback1617Guar: (changeObj: any, change: number) => {
-          var index = vm.data.data.findIndex(function (o) { return o._id === changeObj._id });
-        vm.data.data[index]['16/17 Dollars'] = change;
+        var index = this.data.data.findIndex(function (o) { return o._id === changeObj._id });
+        this.data.data[index]['16/17 Dollars'] = change;
         $scope.$evalAsync();
       },
       clientMix: (changeObj: any, change: number) => {
-      var index = vm.data.data.findIndex(function (o) { return o._id === changeObj._id });
-        vm.data.data[index]['17/18 client mix'] = change;
+        var index = this.data.data.findIndex(function (o) { return o._id === changeObj._id });
+        this.data.data[index]['17/18 client mix'] = change;
         $scope.$evalAsync();
       },
       effectiveROC: (changeObj: any, change: number) => {
-        var index = vm.data.data.findIndex(function (o) { return o._id === changeObj._id });
-        vm.data.data[index]['17/18 effective ROC'] = change;
+        var index = this.data.data.findIndex(function (o) { return o._id === changeObj._id });
+        this.data.data[index]['17/18 effective ROC'] = change;
         $scope.$evalAsync();
       }
     }
